04a9e719b4f8f23d7446a2210b2482a1136d4413
adnene-guessoum <85586056+adnene-guessoum@users.noreply.github.com>
2022-08-09 11:42:37 +0200

    initial commit for github

---
+++ api.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 24 16:10:30 2022
+
+@author: Adnene
+"""
+
+import flask
+import json
+import requests
+
+from tools.preprocess import cleaning
+import pandas as pd
+import xgboost
+import pickle
+
+
+#construction API Flask: api de prediction interrogé
+#par le dashboard streamlit qui affichera les résultats
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+
+#page d'accueil du site
+@app.route('/')
+def index():
+    # A welcome message to test our server
+    return "<h1>Bienvenu sur l'api Flask du projet 7 scoring credit! Par Adnene.</h1>"
+
+
+# endpoint: prédiction octroie de credit (oui, non pour un client)
+
+#Load Dataframe
+path_df = 'selected_feature_dataset'
+df = pd.read_csv(path_df)
+
+#Load model
+path_model = 'model_final.pickle.dat'
+model = pickle.load(open(path_model,'rb'))
+
+#@app.route('/api/credit/<ID>', methods=['POST'])
+
+@app.route('/api/credit/<int:ID>')
+def predict_credit(ID):     
+    
+    #retourne pour un client si crédit est accordé
+    #ID = int(request.args.get('ID'))
+    data = cleaning(df)
+    
+    data_for_prediction = data[data['ID'] == ID].iloc[:,:-2]
+    data_for_prediction_array = data_for_prediction.values.reshape(1, -1)
+    
+    prediction = model.predict(data_for_prediction_array)
+    proba = model.predict_proba(data_for_prediction_array)
+    
+    dictionnaire = {
+        'individual_data' : data_for_prediction.to_json(),
+        'prediction' : int(prediction),
+        'proba' : float(proba[0][0])
+        }
+
+    print('Nouvelle Prédiction : \n', dictionnaire)
+
+    return flask.jsonify(dictionnaire)
+
+#si ce script est le script lancer:
+if __name__ == "__main__":
+    # url à saisir: http://localhost:5000/ ou http://127.0.0.1:5000
+    app.run()
